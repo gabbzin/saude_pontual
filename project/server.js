483e1908524f3d24bf4b1aab491afdeb3f92410a
Pedro Felipe <pfsilva190406@gmail.com>
2025-04-11 22:17:47 -0300

    Atualização das dependecias e do server.js

--- project/server.js
+++ project/server.js
@@ -2,7 +2,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const port = 3000;
+
 const frontendPath = path.join(__dirname, 'front-end');
 
 app.use(express.static(frontendPath));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'login', 'login.html'));
+});
+
+app.get('/cadastro', (req, res) => {
+    res.sendFile(path.join(frontendPath, 'cadastro', 'cadastro.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
+});
