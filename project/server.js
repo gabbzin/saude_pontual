f1c04428851462718f567968d0a1ab6a5a868513
Pedro Felipe <pfsilva190406@gmail.com>
2025-04-11 22:51:44 -0300

    Criação do dockerfile e alguns comentarios para o server.js

--- project/server.js
+++ project/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// Define a porta onde o servidor vai rodar
 const port = 3000;
 
 const frontendPath = path.join(__dirname, 'front-end');
 
 app.use(express.static(frontendPath));
 
+// Define a rota principal '/'
 app.get('/', (req, res) => {
     res.sendFile(path.join(frontendPath, 'login', 'login.html'));
 });
