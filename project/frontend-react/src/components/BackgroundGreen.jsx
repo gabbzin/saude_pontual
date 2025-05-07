9b66f71cff0c539ee76ce14514dbfabaf249aaa4
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-05-07 15:10:10 -0300

    Criação do componente BackgroundGreen

---
+++ project/frontend-react/src/components/BackgroundGreen.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function BackgroundGreen({imageUrl}){
+
+    const BackgroundStyle = {
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        backgroundImage: `url(${imageUrl})`,
+        backgroundPosiiton: "center",
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        zIndex: -1,
+    }
+
+    return <div style={BackgroundStyle}/>
+}
