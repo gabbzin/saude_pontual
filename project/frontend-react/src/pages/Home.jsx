6117e76d6f1c21e258ae50b2d68f9aac86f00688
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-05-09 18:50:38 -0300

    Estruturação Incompleta da tela Home

--- project/frontend-react/src/pages/Home.jsx
+++ project/frontend-react/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Background from "../components/Background";
 import Button from "../components/Button";
+import Calendar from "../components/calendar";
 
 import FundoLaranja from "../assets/background_orange.jpg";
 import Logo from "../assets/logo_saude_pontual.png";
@@ -10,62 +11,71 @@ import "../styles/home.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home(){
+
+    const resolucaoLogoXY = 200;
+    const resolucaoProfileXY = 85;
+
     return (
-        <div className="container flex" style={{
-                "height": "100vh",
-                "fontFamily": "sans-serif"
-            }}>
+        <div id="container">
             <Background imageUrl={FundoLaranja}/>
 
-            <aside className="sidebar">
+            <header id="caixabranca">
+                <Button />
+            </header>
+
+            <div className="content">
+                <aside className="sidebar">
                 
-                <div className="logo-area">
-                    <img src={Logo} alt="Logo" />
-                    <h2>SAÚDE PONTUAL</h2>
-                </div>
-
-                <p className="descricao">
-                    Nosso propósito é simplificar sua jornada de cuidados,
-                    oferecendo acesso rápido a informações sobre medicamentos,
-                    agendamentos de consultas e acompanhamento médico.
-                    Com nossa plataforma, conectamos você aos profissionais de
-                    saúde de forma ágil e segura, sempre com o apoio da UNICEPLAC.
-
-                    <br />
-
-                    Obrigado por confiar em nós!
-                </p>
-
-                <Button 
-                    id={"historico"}
-                    text={"Histórico de Consultas"}
-                    onClick={() => {
-                        console.log("Abrir Histórico")
-                    }}
-                    style={{
-
-                    }}
-                />
-
-            </aside>
-
-            <main className="main-content">
-                <div className="header">
-                    <img className="avatar" src={ProfileIcon} alt="Foto de Perfil" />
-                    <h1>SEJA BEM-VINDA, THALITA KAMILLE!</h1>
-                </div>
-                <div id="calendar"></div>
-                <div className="actions">
-                    <Button 
-                        id={"schedule"}
-                        text={"Agendar Consulta"}
+                    <div className="d-grid">
+                        <img src={Logo} alt="Logo" width={resolucaoLogoXY} height={resolucaoLogoXY} style={{margin: "auto"}}/>
+                        <h2 className="text-center">SAÚDE PONTUAL</h2>
+                    </div>
+                    <p className="description">
+                        Nosso propósito é simplificar sua jornada de cuidados,
+                        oferecendo acesso rápido a informações sobre medicamentos,
+                        agendamentos de consultas e acompanhamento médico.
+                        Com nossa plataforma, conectamos você aos profissionais de
+                        saúde de forma ágil e segura, sempre com o apoio da UNICEPLAC.
+                        <br />
+                        Obrigado por confiar em nós!
+                    </p>
+                    <Button
+                        id={"historico"}
+                        text={"Histórico de Consultas"}
+                        onClick={() => {
+                            console.log("Abrir Histórico")
+                        }}
+                        style={{
+                        }}
                     />
-                    <Button 
-                        id={"Profile"}
-                        text={"Ir ao perfil"}
-                    />
-                </div>
-            </main>
+                </aside>
+                <main className="main-content">
+                    <div className="header flex">
+                        <img className="avatar" src={ProfileIcon} alt="Foto de Perfil"
+                            width={resolucaoProfileXY} height={resolucaoProfileXY}
+                            style={{
+                                borderRadius: "50%"
+                            }}
+                        />
+                        <h1>SEJA BEM-VINDA, THALITA KAMILLE!</h1>
+                    </div>
+                    <div id="calendar" style={{maxHeight: "100vh"}}>
+                        <Calendar/>
+                    </div>
+                    <div className="actions">
+                        <Button
+                            id={"schedule"}
+                            text={"Agendar Consulta"}
+                            onClick={() => {console.log("Agendar Consulta")}}
+                        />
+                        <Button
+                            id={"profile"}
+                            text={"Ir ao perfil"}
+                            onClick={() => {console.log("Ir ao perfil")}}
+                        />
+                    </div>
+                </main>
+            </div>
         </div>
     );
 }
