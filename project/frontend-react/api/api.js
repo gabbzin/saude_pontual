6f6fb4838f6c5adad608a610c9efcb097b52c2cb
Pedro Felipe <pfsilva190406@gmail.com>
2025-05-12 20:27:43 -0300

    api para o frontend

---
+++ project/frontend-react/api/api.js
@@ -0,0 +1,17 @@
+export async function cadastrarUsuario(dados) {
+  const res = await fetch(`http://localhost:3001/api/usuarios`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(dados)
+  });
+  return res.json();
+}
+
+export async function loginUsuario(dados) {
+  const res = await fetch(`http://localhost:3001/api/login`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(dados)
+  });
+  return res.json();
+}
