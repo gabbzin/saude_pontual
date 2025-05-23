c1f90013adc93e2c534e8a14dc7f1386f7d95ef2
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-05-23 00:37:08 -0300

    Implementação das rotas com verificação via token

--- project/frontend/src/routes/routes.jsx
+++ project/frontend/src/routes/routes.jsx
@@ -1,24 +1,40 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
 import Login from "../pages/Login";
 import Cadastro from "../pages/Cadastro";
 import Home from "../pages/Home";
 import FichaPessoa from "../pages/FichaPessoa";
 import FichaPet from "../pages/FichaPet";
+import { AuthContext } from "../contexts/AuthContext";
 
 const AppRoutes = () => {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/cadastro" element={<Cadastro />} />
-        <Route path="/" element={<Home />} />
-        <Route path="/fichapessoa" element={<FichaPessoa />} />
-        <Route path="/fichapet" element={<FichaPet />} />
-        <Route path="*" element={<Login />} />
-      </Routes>
-    </Router>
-  );
+    const {usuario, carregando} = useContext(AuthContext);
+
+    if (carregando){
+        return <div>Segura a emoção que tá carregando</div>
+    }
+
+    const redirectToLogin = <Navigate to="/login"/>;
+
+    return (
+        <Router>
+            <Routes>
+                {/* Rotas de Cadastro e Login */}
+                <Route path="/login" element={<Login />} />
+                <Route path="/cadastro" element={<Cadastro />} />
+
+                {/* Rotas Protegidas (Acessíveis apenas com o token) */}
+                <Route path="/" element={usuario ? <Home /> : redirectToLogin} />
+                <Route path="/fichapessoa" element={usuario ? <FichaPessoa /> : redirectToLogin} />
+                <Route path="/fichapet" element={usuario ? <FichaPet /> : redirectToLogin} />
+
+                {/* Rota genérica */}
+                <Route path="*" element={redirectToLogin} />
+            </Routes>
+        </Router>
+    );
 };
 
 export default AppRoutes;
