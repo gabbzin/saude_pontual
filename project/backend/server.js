159987dee6d7e49495b4f66f99c24d2c5d96978b
Pedro Felipe <pfsilva190406@gmail.com>
2025-06-11 19:48:42 -0300

    rota de /health check

--- project/backend/server.js
+++ project/backend/server.js
@@ -5,7 +5,7 @@ const usuarioRoutes = require("./routes/usuarioRoutes");
 const consultaPetRoutes = require("./routes/consultaPetRoutes"); // Importar as novas rotas de consulta de pet
 const profissionalRoutes = require("./routes/profissionalRoutes"); // Importar as rotas de profissionais
 const consultaRoutes = require("./routes/consultaRoutes"); // Importar as novas rotas de consulta
-const {db, createTables} = require("./db");
+const { db, createTables } = require("./db");
 
 require("dotenv").config();
 
@@ -13,6 +13,11 @@ const app = express();
 
 // Define a porta onde o servidor vai rodar
 const port = 3001;
+
+// health check
+app.use("/health", (req, res) => {
+  res.status(200).json({ message: "Servidor está rodando!" });
+});
 
 app.use(cors());
 app.use(express.json());
@@ -22,18 +27,21 @@ app.use("/api", profissionalRoutes); // Usar as rotas de profissionais
 app.use("/api", consultaRoutes);
 
 async function startServer() {
-    try {
-        // Inicializa a criação/atualização das tabelas e aguarda a conclusão
-        await createTables();
-        
-        // Inicia o servidor apenas após a configuração bem-sucedida do banco de dados
-        app.listen(port, () => {
-            console.log(`Servidor rodando em http://localhost:${port}`);
-        });
-    } catch (error) {
-        console.error("Falha ao inicializar o servidor (erro na criação/atualização de tabelas):", error);
-        process.exit(1); // Encerrar o processo se as tabelas não puderem ser configuradas
-    }
+  try {
+    // Inicializa a criação/atualização das tabelas e aguarda a conclusão
+    await createTables();
+
+    // Inicia o servidor apenas após a configuração bem-sucedida do banco de dados
+    app.listen(port, () => {
+      console.log(`Servidor rodando em http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error(
+      "Falha ao inicializar o servidor (erro na criação/atualização de tabelas):",
+      error,
+    );
+    process.exit(1); // Encerrar o processo se as tabelas não puderem ser configuradas
+  }
 }
 
 startServer();
