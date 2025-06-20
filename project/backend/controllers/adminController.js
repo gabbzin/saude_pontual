f9b0d132a3469f7c53818387d937c2648086b06a
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-06-20 17:54:28 -0300

    Criação do Controller do Admin

---
+++ project/backend/controllers/adminController.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const adminEmail = process.env.ADMIN_EMAIL;
+const adminSenha = process.env.ADMIN_SENHA;
+const jwtSecret = process.env.JWT_CHAVE;
+
+async function loginAdmin(req, res) {
+    const { email, senha } = req.body;
+
+    if (email === adminEmail && senha === adminSenha) {
+        const token = jwt.sign({ role: "admin", email }, jwtSecret, {
+            expiresIn: "1h",
+        });
+
+        return res.json({ token });
+    }
+
+    return res.status(401).json({error: "Credenciais Inválidas"});
+}
+
+module.exports = {loginAdmin}
