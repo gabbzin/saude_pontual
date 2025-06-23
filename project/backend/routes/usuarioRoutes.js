b9cd84cf05d558bcd5eb0a879f9d64903da2f174
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2025-06-23 14:45:46 -0300

    Corrige importação da função pegarPerfil: altera para pegarPerfilDoToken no arquivo usuarioRoutes.js

--- project/backend/routes/usuarioRoutes.js
+++ project/backend/routes/usuarioRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const {
     cadastrarUsuario,
     loginUsuario,
-    pegarPerfil,
+    pegarPerfilDoToken,
     adicionarInfoPerfil,
     listarTodosUsuarios
 } = require("../controllers/usuarioController");
