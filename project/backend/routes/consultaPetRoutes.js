8714fdb01d3e1da1d4ab3197ae43e94558399e9b
Pedro Felipe <pfsilva190406@gmail.com>
2025-06-26 22:45:20 -0300

    retorna com a consulta do pet

--- project/backend/routes/consultaPetRoutes.js
+++ project/backend/routes/consultaPetRoutes.js
@@ -1,15 +1,11 @@
-// const express = require('express');
-// const router = express.Router();
-// const petConsultaController = require('../controllers/petConsultaController');
-// const { verificarToken } = require('../middleware/authMiddleware'); // Exemplo de middleware de autenticação
+const express = require('express');
+const router = express.Router();
+const petConsultaController = require('../controllers/petConsultaController');
+const { verifyToken } = require('../middleware/authMiddleware');
 
-// // Rota para criar consulta de pet (protegida)
-// router.post('/consultas-pet', verificarToken, petConsultaController.criarConsultaPet);
+// Rota para criar consulta de pet (protegida)
+router.post('/pet/fichapet', verifyToken, petConsultaController.criarConsultaPet);
 
-// // Rota para listar consultas de pet do usuário (protegida)
-// router.get('/consultas-pet/me', verificarToken, petConsultaController.listarConsultasPetUsuario);
-
-// // Rota para listar todas as consultas de pet (para admin/profissional, ajuste o middleware conforme necessário)
-// router.get('/consultas-pet/all', verificarToken, petConsultaController.listarTodasConsultasPet); // ou um middleware mais específico para admin/profissional
-
-// module.exports = router;
+// Rota para listar consultas de pet do usuário (protegida)
+router.get('/pet/consultas', verifyToken, petConsultaController.listarConsultasPetUsuario);
+module.exports = router;
